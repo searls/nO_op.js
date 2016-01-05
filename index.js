652b8be299507f258782212faee330598d40011f
Justin Searls <searls@gmail.com>
2016-01-05 15:22:41 -0500

    Nail the primary use case.

---
+++ index.js
@@ -0,0 +1,10 @@
+// Dump globals everywhere.
+global.o = global.o || {}
+o.O = undefined
+global.O = global.O || {}
+O.o = undefined
+global.o_O = undefined
+global.O_o = undefined
+
+// It wouldn't feel very no-op'ey if require actually returned something.
+module.exports = undefined
